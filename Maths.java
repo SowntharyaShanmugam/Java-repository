4a0df2bdc1420a8d7de4ecb15b91f8ca27f076e9
SowntharyaShanmugam <sowntharya3097@gmail.com>
2019-07-04 12:58:21 +0530

    Third commit

---
+++ Maths.java
@@ -0,0 +1,22 @@
+public class Maths
+{
+ public static void main(String args[])
+ {
+ int i1=27;
+ int i2=-45;
+ double d1=84.6;
+ System.out.println("Absolute value of i1:"+Math.abs(i1)); 
+ System.out.println("Absolute value of d1:"+Math.abs(d1));
+ System.out.println("Round off d1:"+Math.round(d1));
+ System.out.println("Ceiling of d1:"+Math.ceil(d1));
+ System.out.println("Floor of d1:"+Math.floor(d1));
+ System.out.println("Min of '"+i1+"'and'"+i2+"':"+Math.min(i1,i2));
+ System.out.println("Max of '"+i1+"'and'"+i2+"':"+Math.max(i1,i2));
+ }
+}
+ 
+ 
+ 
+ 
+ 
+ 
